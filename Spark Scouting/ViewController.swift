dd61979df7d0f007e72c8087a0b1430ecf47ed9c
Ajay Sharma <ajayks@msn.com>
2018-11-01 19:46:28 -0400

    Initial Commit

---
+++ Spark Scouting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spark Scouting
+//
+//  Created by Ajay Sharma on 2018-11-01.
+//  Copyright © 2018 Spark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
